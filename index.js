14f8cf49ac6c2774fe3f6cc309b3cdb965bb2bf6
joyaljosz <joyaljose.tech@gmail.com>
2024-04-23 11:30:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const city = document.querySelector('.city');
+const temp = document.querySelector('.temp');
+const humid = document.querySelector('.humidity');
+const wind = document.querySelector('.wind');
+
+const bar = document.getElementById('srhbar');
+const search = document.getElementById('search');
+
+async function getWeather(data) {
+    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=9df883405da44f6686d90454242204&q=${data}&aqi=no`);
+    const res = await response.json();
+
+    setWeather(res);
+}
+
+function setWeather(data) {
+    city.innerHTML = `<h3>City:${data.location.name}</h3>`
+    temp.innerHTML = `<h3>Temparature:${data.current.temp_c}</h3>`;
+    humid.innerHTML = `<h3>Humidity:${data.current.humidity}</h3>`;
+    wind.innerHTML = `<h3>Wind:${data.current.wind_kph}</h3>`;
+}
+
+function start(){
+    getWeather(bar.value);
+}
